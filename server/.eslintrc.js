a28ecb8c2dcf702f9395e02e591c2d47f012e741
Rowan Nadon <rowan.nadon@stonybrook.edu>
2024-03-27 20:16:31 -0400

    formatting

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,36 +1,35 @@
 module.exports = {
-    env: {
-      node: true,
-      browser: true,
-      es2021: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false,
     },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: false,
-      },
-    },
-    plugins: ['prettier', '@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      'no-console': 'off',
-    },
-    globals: {
-      Edit: 'writable',
-      console: 'writable',
-      _: 'writable',
-      $: 'writable',
-    },
-  };
-  
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+  globals: {
+    Edit: 'writable',
+    console: 'writable',
+    _: 'writable',
+    $: 'writable',
+  },
+};
