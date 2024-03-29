4279e8e8e25aa39340f20070a01100db6ebbfd07
Rowan Nadon <rowan.nadon@stonybrook.edu>
2024-03-29 17:16:56 -0400

    test deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div className="text-center selection:bg-green-900">
       <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <h1>Test Page</h1>
+        <h1>Test Deployment</h1>
         <div className="mt-3">
           <button
             type="button"
