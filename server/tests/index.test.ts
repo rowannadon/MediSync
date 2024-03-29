78af341accbd9f993a5b1388a0f91d5f764b33d6
Daniel Lin <daniel.lin@stonybrook.edu>
2024-03-29 16:41:26 -0400

    backend tests

---
+++ server/tests/index.test.ts
@@ -0,0 +1,20 @@
+const request = require('supertest');
+import app from '../src/index';
+
+describe('GET /test', () => {
+
+  test('responds with message from server', async () => {
+    const response = await request(app).get('/test');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ test: 'This is a response from the server!' });
+  });
+});
+
+describe('GET /health', () => {
+
+  test('responds with message from server', async () => {
+    const response = await request(app).get('/health');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: 'healthy' });
+  });
+});
