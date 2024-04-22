f56d8b029590d46021a5ed4e824cbcdc027f6298
Amatwey <Alexander.Matwey@stonybrook.edu>
2024-04-21 22:18:09 -0400

    Fix Cypress Paths

--- cypress/e2e/testSocket.cy.js
+++ cypress/e2e/testSocket.cy.js
@@ -3,5 +3,8 @@ describe('socket Button', () => {
     cy.visit('http://localhost:5173/test')  
   cy.get('button').contains('Get socket.io Response').click()
   cy.get('p[id="socket.io-response"]').contains('socket.io response: This is a response from the server!')
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
   })
 })
