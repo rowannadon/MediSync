940c6ac8aaf1a4eb1b6f1966b474194abe11a8ce
Rowan Nadon <rowan.nadon@stonybrook.edu>
2024-05-08 18:46:35 -0400

    login error message

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -13,17 +13,19 @@ import {
 } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { navigate } from 'wouter/use-browser-location';
+import { X } from 'lucide-react';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const loginFunctions = useAuth();
 
   return (
     <div className="flex h-screen w-screen flex-row bg-secondary">
       <NavMenu />
       <div className="flex h-screen w-screen flex-col items-center justify-center">
-        <Card className="flex w-[400px] flex-col space-y-4" data-cy="LoginForm">
+        <Card className="flex w-[400px] flex-col" data-cy="LoginForm">
           <CardHeader className="space-y-1">
             <CardTitle className="text-2xl font-bold">Login</CardTitle>
           </CardHeader>
@@ -51,7 +53,19 @@ const LoginPage = () => {
               />
             </div>
           </CardContent>
-          <CardFooter>
+          <CardFooter className="flex flex-col">
+            {errorMessage && (
+              <div className="mb-4 flex w-full flex-row justify-between rounded-lg bg-[#ff5555] p-3">
+                {errorMessage}
+                <X
+                  className="text-white"
+                  onClick={() => {
+                    setErrorMessage('');
+                  }}
+                />
+              </div>
+            )}
+
             <Button
               className="w-full rounded bg-primary p-2 text-white"
               onClick={() => {
@@ -65,7 +79,7 @@ const LoginPage = () => {
                     }, 500);
                   })
                   .catch((err: any) => {
-                    console.error(err);
+                    setErrorMessage('Invalid username or password');
                   });
               }}
             >
